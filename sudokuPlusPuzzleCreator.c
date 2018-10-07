2793a78756cb39ce2d00f2f1bd37851b6aa27b56
Aniket Sanghi <mysoftwarehub@sanghi.local>
2018-10-07 14:36:37 +0530

    Added comment to expain code better and added a README.txt file

--- sudokuPlusPuzzleCreator.c
+++ sudokuPlusPuzzleCreator.c
@@ -1,26 +1,57 @@
+/*                     -------------------------------
+                       |  A SUDOKU+ puzzle Generator |
+                       -------------------------------
+																	--- made by Aniket Sanghi as an assignment under Prof. Subhajit Roy
+
+A sudoku+ is a sudoku puzzle with normal sudoku constraints added with a constraint that in each diagonal, no 2 cells can have same digit
+
+HOW TO RUN:
+-----------
+
+						./sudokuPlusPuzzleCreator puzzle4.txt
+
+where,  puzzle4.txt is a file where you want your new puzzle to be written down.
+
+
+
+Preposition encoding:
+--------------------
+
+Preposition --- Aijk, where i represents the digit placed in jk(th) cell
+
+
+Variables can be feeded in 1-729 number format so ijk converted to number by   (i-1)*81 + (j-1)*9 + k !
+
+*/
+
+
+
+//----------  Code Begins --------//
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
+//A 2D array to store puzzle values
 int Puzzle[9][9];
 
 
-//Checking if a particular sudoku puzzle is solvable
+//Checking if a particular sudoku puzzle is solvable (A particular logic statement is satisfiable)
 int satisfy()
 {
 	char satisfiability[10];
 
+	//Read the miniSAT output file and read the first line which say about SAT or UNSAT
 	FILE *solution = fopen("minisat_outputFile.txt","r");
 	fgets(satisfiability,10,solution);
 	fclose(solution);
 
-	if(satisfiability[0]=='U') return 0;
+	if(satisfiability[0]=='U') return 0;    //If it says UNSAT then return 0 (not Satisfiable) 
 	else return 1;
 
 }
 
-//Write the puzzle in a file
+//A function that will write our 2D array puzzle into the file following that . representation
 void WritePuzzleInFile(char *filename)
 {
 	FILE *puzzle = fopen(filename, "w");
@@ -45,7 +76,7 @@ void WritePuzzleInFile(char *filename)
 }
 
 
-//Function to Convert returned output of minisat and print the solution of puzzle
+//Function to Convert returned output of minisat in the 2D array format
 void DecodeOutputToPuzzleSolution()
 {
 	char satisfiability[10];
@@ -53,7 +84,7 @@ void DecodeOutputToPuzzleSolution()
 
 	FILE *solution = fopen("minisat_outputFile.txt","r");
 
-	fgets(satisfiability,10,solution);
+	fgets(satisfiability,10,solution);      //Get satisfiability value
 	if(satisfiability[0]!='U')
 	{
 		for(int i=0;i<729;++i)
@@ -62,7 +93,7 @@ void DecodeOutputToPuzzleSolution()
 
 			if(data>0)
 			{
-				Puzzle[((data-1)%81)/9][((data-1)%81)%9] = (data-1)/81 + 1;
+				Puzzle[((data-1)%81)/9][((data-1)%81)%9] = (data-1)/81 + 1;     //converting 1-729 values of variables to corresponding 2D array form
 			}
 		}
 
@@ -74,48 +105,49 @@ void DecodeOutputToPuzzleSolution()
 
 
 
-//Function 
+//Function to generate a random puzzle which is satisfiable (may have multiple solution) to get a initial filled sudoku+
 void generateRandomPartOfSolvedPuzzle(char *filename)
 {
-	srand(time(0));
-	int x;
-	int c;
-	c = rand() % 10 + 1;
+	srand(time(0));                             //Setting the seed of rand function with respect to system time
+	int x,c;
+	c = rand() % 10 + 1;						//Choosing that how many numbers to be taken true initially (out of 729 variables)
 
 
 	while(c--)
 	{
 		
 		x = rand() % 1000;
-		while(x>=729) x = rand() % 1000;
+		while(x>=729) x = rand() % 1000;				//Until we get a number less than 729
 
-		Puzzle[((x)%81)/9][((x)%81)%9] = (x)/81 + 1;
+		Puzzle[((x)%81)/9][((x)%81)%9] = (x)/81 + 1;	//Decoding that number to give the puzzle grid its value
 
 	}
 
-	WritePuzzleInFile(filename);
+	WritePuzzleInFile(filename);						//Write in the file which will be feeded to sudokuPlusSolver.c
 	
 }
 
-//Generating A Random Solved Sudoku Puzzle
+
+//Generating A Random Solved Sudoku Puzzle using the Random solvable puzzle
 void generateRandomSolvedPuzzle(char *filename)
 {
 	char execute[1000] = "./sudokuPlusSolver ";
-	strcat(execute,filename);
+	strcat(execute,filename);							// execute = command to run sudokuPlusSolver with the puzzle file
 
-	generateRandomPartOfSolvedPuzzle(filename);
-	system(execute);
+	//generate the Random sudoku puzzle 
+	generateRandomPartOfSolvedPuzzle(filename);			
+	system(execute);									//Try to solve it
 
-	while(!satisfy())
+	while(!satisfy())									//Until we get a puzzle that is solvable
 	{
 
-		for(int i=0;i<9;++i) for(int j=0;j<9;++j) Puzzle[i][j]=0;
+		for(int i=0;i<9;++i) for(int j=0;j<9;++j) Puzzle[i][j]=0;	//Re-initialise 2D array puzzle grid
 	
-		generateRandomPartOfSolvedPuzzle(filename);
+		generateRandomPartOfSolvedPuzzle(filename);		//Again generate random Sudoku puzzle question until we get a solvable puzzle
 		system(execute);
 	}
 
-	DecodeOutputToPuzzleSolution();
+	DecodeOutputToPuzzleSolution();						//Write the solved puzzle in the 2D grid and file 
 
 	WritePuzzleInFile(filename);
 
@@ -140,12 +172,12 @@ int NumberOfSolutions()
 		if(sat[0]=='U') return count;
 		else
 		{
-			if(count==2) return count;
+			if(count==2) return count;					//We just need to check multiple solution so this limit of 2 is set. If you want actual figure then comment this line
 
 			for(int i=0;i<729;++i)
 			{
-				fscanf(output,"%d",&data);
-				fprintf(input,"%d ",-data);
+				fscanf(output,"%d",&data);				// Keep on writing negation of our answer to input file 
+				fprintf(input,"%d ",-data);				// so as to get a new interpretation that gives true
 				
 			}
 			fprintf(input,"0\n");
@@ -171,7 +203,8 @@ int NumberOfSolutions()
 	return count;
 }
 
-//
+
+//Function which checks if our current puzzle can further be reduced
 int verify(char *filename)
 {
 	char execute[1000] = "./sudokuPlusSolver ";
@@ -179,34 +212,36 @@ int verify(char *filename)
 	int answer = 1;
 	int numberOfSolutions = 0;
 
+	//Looping over all grid values
 	for(int i=0;i<9;++i)
 	{
 		for(int j=0;j<9;++j)
 		{
-			if(Puzzle[i][j]!=0)
+			if(Puzzle[i][j]!=0)								//Values which are not zero
 			{
 				int temp = Puzzle[i][j];
-				Puzzle[i][j] = 0;
-				WritePuzzleInFile(filename);
-				system(execute);
-				numberOfSolutions = NumberOfSolutions();
-				if(numberOfSolutions == 1)
+				Puzzle[i][j] = 0;							// Check
+				WritePuzzleInFile(filename);				// If upon removing any value
+				system(execute);							// The puzzle still give one solution
+				numberOfSolutions = NumberOfSolutions();	// Then return 1 implying we can further shorten the puzzle
+				if(numberOfSolutions == 1)					
 					return 1;
 				Puzzle[i][j] = temp;
 				WritePuzzleInFile(filename);
 			}
 		}
 	}
-
+	// If it give multiple solution upon removing any value then return 0
 	return 0;
 
 }
 
 
 
-
+//Function to generate minimal puzzle that has exactly 1 solution using our RandomSolvedPuzzle.
 void generatePuzzle(char *filename)
 {
+	srand(time(0));
 	int a,b,temp,numberOfSolutions;
 	char execute[1000] = "./sudokuPlusSolver ";
 	strcat(execute,filename);
@@ -215,22 +250,26 @@ void generatePuzzle(char *filename)
 	while(1)
 	{
 		a = 100;b = 100;
-		while(a>8) a = rand() % 10;
-		while(b>8) b = rand() % 10;
+		while(a>8) a = rand() % 10;									// consider random row
+		while(b>8) b = rand() % 10;									// And random column
 
-		while(Puzzle[a][b]==0)
+		while(Puzzle[a][b]==0)										//Until at that value we have an element (1-9)
 		{
 			a = 100;b = 100;
 			while(a>8) a = rand() % 10;
 			while(b>8) b = rand() % 10;
 		}
 
+		//Remove that value from that cell
 		temp = Puzzle[a][b];
 		Puzzle[a][b] = 0;
+
+		//Then check if now the puzzle still gives unique solution
 		WritePuzzleInFile(filename);
 		system(execute);
 		numberOfSolutions = NumberOfSolutions();
-		printf("\n\n%d\n\n",numberOfSolutions);
+
+		//If it doesn't give unique solution then don't remove this element and check if it is the final solution!?
 		if(numberOfSolutions != 1)
 		{
 			 Puzzle[a][b] = temp;
@@ -240,6 +279,24 @@ void generatePuzzle(char *filename)
 	}
 }
 
+
+//Function to print puzzle
+void printingPuzzle()
+{
+	printf("\n\n\n # The Randomly generated minimal sudoku+ puzzle with unique solution \n\n\n");
+	for(int i=0;i<9;++i)
+		{
+			for(int j=0;j<9;++j)
+			{
+				if(Puzzle[i][j]==0)
+					printf(".");
+				else
+					printf("%d",Puzzle[i][j]);
+				if(j!=8) printf(" ");
+			}
+			printf("\n");
+		}
+}
 
 int main(int argc, char* argv[])
 {
@@ -255,6 +312,9 @@ int main(int argc, char* argv[])
 
 	//Generating a Question!
 	generatePuzzle(filename);
+
+	//Output the puzzle
+	printingPuzzle();
 
 	return 0;
 
--- sudokuPlusSolver.c
+++ sudokuPlusSolver.c
@@ -1,30 +1,59 @@
+/*          		------------------------   
+					|	A SUDOKU+ Solver   |
+					------------------------
+														---	made by Aniket Sanghi as an assignment given by Prof. Subhajit Roy
+
+A sudoku plus is a puzzle with normal sudoku constraints added with constraint that each diagonal cannot have same numbers in any 2 of their cells.
+
+HOW TO RUN:
+------------
+
+                            ./sudokuPlusSolver puzzlequestion.txt
+
+Where, puzzlequestion.txt is a file where the puzzle is stored.
+
+
+Preposition encoding:
+--------------------
+
+Preposition ---  Aijk, where i represents the digit placed in jk(th) cell
+
+
+Variables can be feeded in 1-729 number format so ijk converted to number by   (i-1)*81 + (j-1)*9 + k !
+
+*/
+
+
+//--------  The Code Begins ---------//
+
 #include <stdio.h>
 #include <stdlib.h>
 
+//Defining a cell in a sudoku box (To be used to apply BoxCondition())
 struct cell
 {
 	int r;
 	int c;
 };
 
+//Declaring a 2D array signifying Sudoku puzzle
 int Puzzle[9][9];
 
 
 //Function to encode data from given puzzle to corresponding extra clauses 
 void EncodePuzzleData(char *filename)
 {
-	long long int count = 0; //To count the no. of extra clauses needed using the data given in puzzle
-	char data;    //variable to read data from file
+	long long int count = 0; 		//To count the no. of extra clauses needed using the data given in puzzle
+	char data;    					//variable to read data from file
 
 	//Read Puzzle data and append the corresponding encoding of the input to clauses in minisat input file
-	
 	FILE *readpuzzle = fopen(filename,"r");
-	if(readpuzzle == NULL) {printf("\n\nNo such file exist. Considering empty sudoku\n\n");return;}
+	if(readpuzzle == NULL) {	printf("\n\nNo such file exist. Considering empty sudoku\n\n");	return;	} 	//If File doesn't exist then consider empty sudoku
 	FILE *minisatInput = fopen("minisat_inputFile.txt","w");
 
-	fprintf(minisatInput,"p cnf 729 12393\n");   //Telling that it is in cnf and giving information about no. of clauses and variables
+	fprintf(minisatInput,"p cnf 729 12393\n");   //Editing the first line in DIMACS representation
 
-    //Reading data and appending clauses from given data
+    //Reading data from puzzle file and appending clauses from given data
 	for(int i=0;i<9;++i)
 	{
 		for(int j=0;j<9;++j)
@@ -42,13 +71,26 @@ void EncodePuzzleData(char *filename)
 			fscanf(readpuzzle,"%c",&data);
 		}
 	}
+
 	//Editing the number of clauses based on extra clauses from given data
 	fseek(minisatInput,9,SEEK_SET);
 	fprintf(minisatInput," %lld",12393+count);
+
 	//Closing files
 	fclose(readpuzzle);
 	fclose(minisatInput);
 }
+
+/*
+
+In the following funcitons in loops 
+r - row    (0-9)
+c - column (0-9)
+d - digit  (0-9)
+
+*/
+
+
 
 //Function to write clauses that describes that every cell should have atleast one number
 void CellCondition1()
@@ -61,7 +103,7 @@ void CellCondition1()
 		for(int c=0;c<9;++c)
 		{
 			for(int d=0;d<9;++d)
-				fprintf(minisatInput,"%d ", (d*81 + r*9 + c+1));    //Condition that every cell should contain atleast one digit
+				fprintf(minisatInput,"%d ", (d*81 + r*9 + c+1));           //Condition that every cell should contain atleast one digit
 			fprintf(minisatInput,"0\n");
 		}
 	}
@@ -83,7 +125,7 @@ void CellCondition2()
 			{
 				for(int d2=d1+1;d2<9;++d2)
 				{
-					fprintf(minisatInput, "%d %d 0\n", -(d1*81 + r*9 + c+1) , -(d2*81 + r*9 + c+1));
+					fprintf(minisatInput, "%d %d 0\n", -(d1*81 + r*9 + c+1) , -(d2*81 + r*9 + c+1));  //Condition that a cell can't have 2 numbers
 				}
 			}
 		}
@@ -92,7 +134,9 @@ void CellCondition2()
 	fclose(minisatInput);
 }
 
-//Function to write clauses that tells that no two cells in a row should have same digit
+
+
+//Function to write clauses that tells that no two cells in a row can have same digit
 void RowCondition()
 {
 	FILE *minisatInput = fopen("minisat_inputFile.txt","a");
@@ -106,7 +150,7 @@ void RowCondition()
 			{
 				for(int c2=c1+1;c2<9;++c2)
 				{
-					fprintf(minisatInput, "%d %d 0\n", -(d*81 + r*9 + c1+1) , -(d*81 + r*9 + c2+1));
+					fprintf(minisatInput, "%d %d 0\n", -(d*81 + r*9 + c1+1) , -(d*81 + r*9 + c2+1)); //No 2 cells in a row can have same digit
 				}
 			}
 		}
@@ -114,6 +158,8 @@ void RowCondition()
 
 	fclose(minisatInput);
 }
+
+
 
 //Function to write clauses that tells that no two cells in a column should have same digit
 void ColumnCondition()
@@ -129,7 +175,7 @@ void ColumnCondition()
 			{
 				for(int r2=r1+1;r2<9;++r2)
 				{
-					fprintf(minisatInput, "%d %d 0\n", -(d*81 + r1*9 + c+1) , -(d*81 + r2*9 + c+1));
+					fprintf(minisatInput, "%d %d 0\n", -(d*81 + r1*9 + c+1) , -(d*81 + r2*9 + c+1));  //No 2 cells in a column can have same digit
 				}
 			}
 		}
@@ -137,6 +183,7 @@ void ColumnCondition()
 
 	fclose(minisatInput);
 }
+
 
 //Function to write clauses that tells that no two cells in a box should have same digit
 void BoxCondition()
@@ -146,16 +193,18 @@ void BoxCondition()
 	//Looping over all possible digits
 	for(int d=0;d<9;++d)
 	{
+		//Looping over all 9 boxes
 		for(int ri=0;ri<9;ri+=3)
 		{
 			for(int ci=0;ci<9;ci+=3)
 			{
+				//Converting box to a line of cells!
 				struct cell box[9];
-				for(int i=0;i<9;++i) box[i].r = ri + i/3,box[i].c = ci + i%3;
+				for(int i=0;i<9;++i) box[i].r = ri + i/3,box[i].c = ci + i%3;  //Using the structure cell, I created an array of cells of a box
 
 				for(int i=0;i<9;++i)
 					for(int j=i+1;j<9;++j)
-						fprintf(minisatInput,"%d %d 0\n", -(d*81 + box[i].r*9 + box[i].c+1), -(d*81 + box[j].r*9 + box[j].c+1));
+						fprintf(minisatInput,"%d %d 0\n", -(d*81 + box[i].r*9 + box[i].c+1), -(d*81 + box[j].r*9 + box[j].c+1)); //No two cells are same in that box (converted Array)
 			}
 		}
 	}
@@ -163,11 +212,13 @@ void BoxCondition()
 	fclose(minisatInput);
 }
 
+
 //Function to write clauses that tells that no two cells in a diagonal should have same digit
 void DiagonalCondition()
 {
 	FILE *minisatInput = fopen("minisat_inputFile.txt","a");
 
+	//Looping over all digits
 	for(int d=0;d<9;++d)
 	{
 		for(int i=0;i<9;++i)
@@ -191,15 +242,15 @@ void DiagonalCondition()
 
 
 
-//Function to Convert returned output of minisat and print the solution of puzzle
+//Function to Convert returned output of miniSAT to puzzle form and print the solution of puzzle
 void DecodeOutputToPuzzleSolution()
 {
-	char satisfiability[10];
+	char satisfiability[10];  							//To store Satisfiability value of miniSAT output
 	int data;
 
 	FILE *solution = fopen("minisat_outputFile.txt","r");
 
-	fgets(satisfiability,10,solution);
+	fgets(satisfiability,10,solution);   				//Get value of Satisfiability
 	if(satisfiability[0]!='U')
 	{
 		for(int i=0;i<729;++i)
@@ -231,8 +282,12 @@ void DecodeOutputToPuzzleSolution()
 
 int main(int argc, char *argv[])
 {
-	if(argc==1) return 0;
+	if(argc==1) return 0;    //If No Textfile name entered then return
+
+	//Apply the Additional Clauses based on the given puzzle
 	EncodePuzzleData(argv[1]);
+
+	//Encoding the Normal Clauses following the normal rules of given Sudoku
 	CellCondition1();
 	CellCondition2();
 	RowCondition();
@@ -240,11 +295,11 @@ int main(int argc, char *argv[])
 	BoxCondition();
 	DiagonalCondition();
 
+	//Involing miniSAT to solve the logical argument
 	system("minisat minisat_inputFile.txt minisat_outputFile.txt");
 
+	//Decoding the miniSAT output to our Sudoku Format and showing it in the commandline
 	DecodeOutputToPuzzleSolution();
-
-	
 
 	return 0;
 }
